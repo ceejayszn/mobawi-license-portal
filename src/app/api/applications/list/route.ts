6cb213a627236f1692a15d1d6e8e0524872a4089
ceejayszn <mancollomc@gmail.com>
2026-07-19 18:19:59 +0300

    update

--- src/app/api/applications/list/route.ts
+++ src/app/api/applications/list/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   const apps = await prisma.application.findMany({ select: { id: true, name: true, platform: true }, where: { status: 'Active' } });
   return NextResponse.json(apps);
